1bb4bf92e2a9f8ffd68d6db39a58f205f75a7808
Apple <apple@raghu.local>
2018-09-25 18:27:51 +0530

    remote duplicate folder

--- demo/src/main/java/com/example/demo/repository/DemoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Demo;
-
-public interface DemoRepository extends CrudRepository<Demo, Integer>{
- Demo findByName(String name);
-}
